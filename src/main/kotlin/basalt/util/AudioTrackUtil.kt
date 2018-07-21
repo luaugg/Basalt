53ca7c6888ae42863f57453642a2b5ea255fd600
Sam <ophisbonklol@gmail.com>
2018-07-21 19:07:46 +0100

    gitignore + document util class

--- src/main/kotlin/basalt/util/AudioTrackUtil.kt
+++ src/main/kotlin/basalt/util/AudioTrackUtil.kt
@@ -10,7 +10,24 @@ import net.iharder.Base64
 import org.slf4j.LoggerFactory
 import java.io.IOException
 
+/**
+ * The utility class which converts AudioTracks to easily-transportable data and vice versa.
+ *
+ * @constructor Constructs a new AudioTrackUtil class with an attached [BasaltServer]
+ * @author Sam Pritchard
+ * @since 0.1
+ */
 class AudioTrackUtil internal constructor(private val server: BasaltServer) {
+    /**
+     * Converts a **not-null** AudioTrack into its data representation, which is then used for various purposes.
+     *
+     * <br><p>The data representation is used mainly by clients in order to play media sources, as passing/storing
+     * an entire JSON Object is a little harder and less performant.
+     *
+     * <br>Additionally, Basalt makes no requests in order to resolve the track data as it already has been resolved.
+     * When converting strings via the [toAudioTrack][toAudioTrack] method, similarly, Basalt doesn't have
+     * to make requests in order to decode the track as the data already contains all the information.</p>
+     */
     fun fromAudioTrack(track: AudioTrack): String {
         try {
             val out = FastByteArrayOutputStream()
@@ -21,6 +38,16 @@ class AudioTrackUtil internal constructor(private val server: BasaltServer) {
             throw err
         }
     }
+
+    /**
+     * Converts a **not-null** data representation of an AudioTrack into an AudioTrack, which is then used for various purposes.
+     *
+     * <br><p>This track is only really used internally by Basalt in order to play the raw track
+     * itself (as opposed to the data representation which is used mainly to transport a track object over the net).
+     *
+     * <br>Additionally, and similarly to the [fromAudioTrack][fromAudioTrack] method, Basalt does not have to make any requests to resolve
+     * the track data as it is already contained inside the string.</p>
+     */
     fun toAudioTrack(data: String): AudioTrack {
         try {
             return server.sourceManager.decodeTrack(MessageInput(FastByteArrayInputStream(Base64.decode(data)))).decodedTrack
@@ -29,7 +56,14 @@ class AudioTrackUtil internal constructor(private val server: BasaltServer) {
             throw err
         }
     }
+
+    /**
+     * @suppress
+     */
     companion object {
+        /**
+         * @suppress
+         */
         private val LOGGER = LoggerFactory.getLogger(AudioTrackUtil::class.java)
     }
 }
