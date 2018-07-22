f2234f7d015a8c3baac8701ee2a206f6f4739ee1
Sam <ophisbonklol@gmail.com>
2018-07-22 19:04:58 +0100

    remove newPlayer function + add sequence number

--- src/main/kotlin/basalt/player/SocketContext.kt
+++ src/main/kotlin/basalt/player/SocketContext.kt
@@ -3,10 +3,9 @@ package basalt.player
 import basalt.server.BasaltServer
 import io.undertow.websockets.core.WebSocketChannel
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap
+import java.util.concurrent.atomic.AtomicLong
 
 class SocketContext internal constructor(val server: BasaltServer, val channel: WebSocketChannel, val userId: String) {
     internal val players = Object2ObjectOpenHashMap<String, BasaltPlayer>()
-    fun newPlayer(guildId: String) {
-
-    }
+    internal val seq = AtomicLong(0)
 }
