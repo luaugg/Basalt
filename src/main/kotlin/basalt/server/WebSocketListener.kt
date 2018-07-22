74e05fcbc8526e4654d0c63325798938afdcd058
Sam <ophisbonklol@gmail.com>
2018-07-22 20:35:44 +0100

    massive server changes

--- src/main/kotlin/basalt/server/WebSocketListener.kt
+++ src/main/kotlin/basalt/server/WebSocketListener.kt
@@ -1,12 +1,11 @@
 package basalt.server
 
-import basalt.messages.client.InitializeRequest
-import basalt.messages.client.LoadRequest
-import basalt.messages.client.PlayRequest
+import basalt.messages.client.*
 import basalt.messages.server.DispatchResponse
 import basalt.messages.server.LoadTrackResponse
 import basalt.messages.server.TrackPair
 import basalt.player.AudioLoadHandler
+import basalt.player.BasaltPlayer
 import com.jsoniter.JsonIterator
 import com.jsoniter.output.JsonStream
 import io.undertow.websockets.core.*
@@ -18,7 +17,7 @@ import space.npstr.magma.MagmaServerUpdate
 class WebSocketListener(val server: BasaltServer): AbstractReceiveListener() {
     override fun onClose(webSocketChannel: WebSocketChannel, channel: StreamSourceFrameChannel) {
         super.onClose(webSocketChannel, channel)
-        server.players.remove(webSocketChannel)
+        server.contexts.remove(webSocketChannel)
         val host = webSocketChannel.sourceAddress
                 .toString()
                 .replaceFirst("/", "")
@@ -35,30 +34,74 @@ class WebSocketListener(val server: BasaltServer): AbstractReceiveListener() {
             when (data["op"]!!.toString()) {
                 "initialize" -> {
                     val init = JsonIterator.deserialize(message.data, InitializeRequest::class.java)
+                    val context = server.contexts[channel]
+                    if (context == null) {
+                        LOGGER.error("SocketContext is null for Guild ID: {}\nThis should never happen!", init.guildId)
+                        return
+                    }
+                    context.seq.incrementAndGet()
                     val member = MagmaMember.builder()
                             .guildId(init.guildId)
-                            .userId(server.players[channel]!!.userId)
+                            .userId(context.userId)
                             .build()
                     val update = MagmaServerUpdate.builder()
                             .sessionId(init.sessionid)
                             .endpoint(init.endpoint)
                             .token(init.token)
                             .build()
+                    val basalt = BasaltPlayer(context, init.guildId, server.sourceManager.createPlayer())
+                    context.players[init.guildId] = basalt
                     server.magma.provideVoiceServerUpdate(member, update)
-                    server.magma.setSendHandler(member, AudioSender(server.sourceManager.createPlayer()))
-                    val response = DispatchResponse(guildId = init.guildId, name = "INITIALIZED")
+                    server.magma.setSendHandler(member, AudioSender(basalt.audioPlayer))
+                    val response = DispatchResponse(context, init.guildId, "INITIALIZED")
                     WebSockets.sendText(JsonStream.serialize(response), channel, null)
                     LOGGER.info("Initialized connection from User ID: {} and Guild ID: {}", member.userId, member.guildId)
-
                 }
                 "play" -> {
                     val play = JsonIterator.deserialize(message.data, PlayRequest::class.java)
-                    val member = MagmaMember.builder()
-                            .guildId(play.guildId)
-                            //.userId(server.magma.)
+                    val player = server.contexts[channel]?.players?.get(play.guildId)
+                    if (player == null) {
+                        LOGGER.error("Player or SocketContext is null for Guild ID: {} (Try initializing first!)", play.guildId)
+                        return
+                    }
+                    player.context.seq.incrementAndGet()
+                    player.audioPlayer.playTrack(server.trackUtil.toAudioTrack(play.track))
+                }
+                "stop" -> {
+                    val stop = JsonIterator.deserialize(message.data, StopRequest::class.java)
+                    val player = server.contexts[channel]?.players?.get(stop.guildId)
+                    if (player == null) {
+                        LOGGER.error("Player or SocketContext is null for Guild ID: {} (Try initializing first!)", stop.guildId)
+                        return
+                    }
+                    player.context.seq.incrementAndGet()
+                    player.audioPlayer.stopTrack()
+                }
+                "volume" -> {
+                    val volume = JsonIterator.deserialize(message.data, SetVolumeRequest::class.java)
+                    val player = server.contexts[channel]?.players?.get(volume.guildId)
+                    if (player == null) {
+                        LOGGER.error("Player or SocketContext is null for Guild ID: {} (Try initializing first)!", volume.guildId)
+                        return
+                    }
+                    if (volume.volume < 0 || volume.volume > 1000) {
+                        LOGGER.warn("Volume cannot be negative or above 1000 for User ID: {} and Guild ID: {}",
+                                player.context.userId, volume.guildId)
+                        return
+                    }
+                    player.context.seq.incrementAndGet()
+                    player.audioPlayer.volume = volume.volume
+                    val response = DispatchResponse(player.context, volume.guildId, "VOLUME_UPDATE", volume.volume)
+                    WebSockets.sendText(JsonStream.serialize(response), channel, null)
                 }
                 "loadIdentifiers" -> {
                     val load = JsonIterator.deserialize(message.data, LoadRequest::class.java)
+                    val context = server.contexts[channel]
+                    if (context == null) {
+                        LOGGER.error("SocketContext is null. This should *never* happen.")
+                        return
+                    }
+                    context.seq.incrementAndGet()
                     val identifiers = load.identifiers
                     val list = ObjectArrayList<LoadTrackResponse>(identifiers.size)
                     for (str in identifiers) {
@@ -67,7 +110,7 @@ class WebSocketListener(val server: BasaltServer): AbstractReceiveListener() {
                                 .thenAccept { list.add(it) }
                                 .thenAccept {
                                     if (list.size == identifiers.size) {
-                                        val response = DispatchResponse(name = "IDENTIFY_RESPONSE", data = list.toArray())
+                                        val response = DispatchResponse(context, name = "IDENTIFY_RESPONSE", data = list.toArray())
                                         WebSockets.sendText(JsonStream.serialize(response), channel, null)
                                     }
                                 }
