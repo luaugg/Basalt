826049eeffd2159a3e5fe0437bfb13fe654dfc92
Sam <ophisbonklol@gmail.com>
2018-07-22 21:00:58 +0100

    remove skip request???

--- src/main/kotlin/basalt/messages/client/SkipRequest.kt
+++
@@ -1,8 +0,0 @@
-package basalt.messages.client
-
-import com.jsoniter.annotation.JsonCreator
-import com.jsoniter.annotation.JsonProperty
-
-@Suppress("UNUSED")
-class SkipRequest @JsonCreator constructor(@JsonProperty("op", required = true, nullable = false) val op: String,
-                                           @JsonProperty("guildId", required = true, nullable = false) val guildId: String)
