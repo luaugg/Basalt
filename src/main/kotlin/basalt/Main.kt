f34a530f82c8f20bf3b0362479489e8fa286da52
Sam <ophisbonklol@gmail.com>
2018-07-22 16:12:59 +0100

    add shutdown hook

--- src/main/kotlin/basalt/Main.kt
+++ src/main/kotlin/basalt/Main.kt
@@ -5,4 +5,7 @@ import io.vertx.core.Vertx
 fun main(args: Array<String>) {
     val vertx = Vertx.vertx()
     vertx.deployVerticle("basalt.server.BasaltServer")
+    Runtime.getRuntime().addShutdownHook(Thread {
+        vertx.close()
+    })
 }
