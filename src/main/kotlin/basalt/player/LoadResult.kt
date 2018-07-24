0d3f0c1919e21886340f961c967eadbd29eb90b0
Sam <ophisbonklol@gmail.com>
2018-07-25 00:15:46 +0100

    add loadresult docs

--- src/main/kotlin/basalt/player/LoadResult.kt
+++ src/main/kotlin/basalt/player/LoadResult.kt
@@ -3,6 +3,18 @@ package basalt.player
 import basalt.messages.server.TrackPair
 import org.slf4j.LoggerFactory
 
+/**
+ * A result class which represents a singular, successful load result.
+ *
+ * @property playlistInfo **nullable** pair of information (playlist name + index of selected track)
+ * @property tracks an array of [TrackPair]s
+ * @property result a [ResultStatus] that can never be unknown
+ *
+ * @author Sam Pritchard
+ * @since 1.0
+ * @constructor Constructs a LoadResult from **possibly-null** playlist info, a track array and a [ResultStatus].
+ */
+
 @Suppress("UNUSED")
 class LoadResult(val playlistInfo: Pair<String, Int>?, val tracks: Array<TrackPair>, val result: ResultStatus) {
     init {
@@ -11,7 +23,14 @@ class LoadResult(val playlistInfo: Pair<String, Int>?, val tracks: Array<TrackPa
             throw IllegalArgumentException("ResultStatus should never be equal to UNKNOWN!")
         }
     }
+
+    /**
+     * @suppress
+     */
     companion object {
+        /**
+         * @suppress
+         */
         private val LOGGER = LoggerFactory.getLogger(LoadResult::class.java)
     }
 }
