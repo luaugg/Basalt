81b16ecc62bc21c9d92f668b75494b6bb25df864
Sam <ophisbonklol@gmail.com>
2018-07-24 22:15:38 +0100

    throw exception upon unknown result status

--- src/main/kotlin/basalt/player/LoadResult.kt
+++ src/main/kotlin/basalt/player/LoadResult.kt
@@ -6,8 +6,10 @@ import org.slf4j.LoggerFactory
 @Suppress("UNUSED")
 class LoadResult(val playlistInfo: Pair<String, Int>?, val tracks: Array<TrackPair>, val result: ResultStatus) {
     init {
-        if (result === ResultStatus.UNKNOWN)
+        if (result === ResultStatus.UNKNOWN) {
             LOGGER.error("Result Status is UNKNOWN!")
+            throw IllegalArgumentException("ResultStatus should never be equal to UNKNOWN!")
+        }
     }
     companion object {
         private val LOGGER = LoggerFactory.getLogger(LoadResult::class.java)
