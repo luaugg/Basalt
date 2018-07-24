51327311805987fbf9536354205a5a9f8e6ca3b6
Sam <ophisbonklol@gmail.com>
2018-07-25 00:39:37 +0100

    add AudioLoadHandler docs

--- src/main/kotlin/basalt/player/AudioLoadHandler.kt
+++ src/main/kotlin/basalt/player/AudioLoadHandler.kt
@@ -13,9 +13,35 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 // Credit to Napster (@npstr) for the original Java Completion Code.
 
+/**
+ * Class which loads sources based on provided identifiers asynchronously.
+ *
+ * Each instance of this class can only be used once, and an exception will be thrown as well as an error log
+ * being produced each time this is used more than once -- which it is not, if using the normal, stable Basalt release.
+ *
+ * @property server A [BasaltServer] reference.
+ *
+ * @author Sam Pritchard
+ * @since 1.0
+ * @constructor Constructs an AudioLoadHandler from a [BasaltServer] instance.
+ */
+
 class AudioLoadHandler internal constructor(private val server: BasaltServer): AudioLoadResultHandler {
+    /**
+     * A CompletableFuture that can be completed with a [LoadResult].
+     */
     private val future = CompletableFuture<LoadResult>()
+    /**
+     * An AtomicBoolean that atomically checks to see if this instance has already been used before.
+     */
     private val isUsed = AtomicBoolean(false)
+
+    /**
+     * Loads a source based on a given identifier, returning a CompletionStage that is used internally for various purposes.
+     *
+     * @param identifier The identifier to load.
+     * @return A CompletionStage which eventually completes with a [LoadResult].
+     */
     fun load(identifier: String): CompletionStage<LoadResult> {
         if (isUsed.getAndSet(true)) {
             LOGGER.error("AudioLoadHandler can only be used once!")
@@ -24,9 +50,19 @@ class AudioLoadHandler internal constructor(private val server: BasaltServer): A
         server.sourceManager.loadItem(identifier, this)
         return future
     }
+
+    /**
+     * Called by Lavaplayer whenever a single track is loaded successfully, completing the result.
+     * @param track The AudioTrack object.
+     */
     override fun trackLoaded(track: AudioTrack) {
         future.complete(LoadResult(null, arrayOf(TrackPair(server.trackUtil.fromAudioTrack(track), track)), ResultStatus.TRACK_LOADED))
     }
+
+    /**
+     * Called by Lavaplayer whenever a playlist of tracks (including search results) is loaded successfully, completing the result.
+     * @param playlist The AudioPlaylist object.
+     */
     override fun playlistLoaded(playlist: AudioPlaylist) {
         val (name, selected, type) = if (playlist.isSearchResult)
             Triple(null, -1, ResultStatus.SEARCH_RESULT)
@@ -40,16 +76,38 @@ class AudioLoadHandler internal constructor(private val server: BasaltServer): A
             TrackPair(server.trackUtil.fromAudioTrack(track), track)
         }, type))
     }
+
+    /**
+     * Called by Lavaplayer if no matches could be associated with the provided identifier, completing the result.
+     */
     override fun noMatches() {
         future.complete(NO_MATCHES)
     }
+
+    /**
+     * Called by Lavaplayer whenever an error occurs while loading a source, completing the result.
+     * @param exception The FriendlyException that was thrown.
+     */
     override fun loadFailed(exception: FriendlyException) {
         LOGGER.error("Load failed!", exception)
         future.complete(LOAD_FAILED)
     }
+
+    /**
+     * @suppress
+     */
     companion object {
+        /**
+         * @suppress
+         */
         private val LOGGER = LoggerFactory.getLogger(AudioLoadHandler::class.java)
+        /**
+         * @suppress
+         */
         private val NO_MATCHES = LoadResult(null, emptyArray(), ResultStatus.NO_MATCHES)
+        /**
+         * @suppress
+         */
         private val LOAD_FAILED = LoadResult(null, emptyArray(), ResultStatus.LOAD_FAILED)
     }
 }
