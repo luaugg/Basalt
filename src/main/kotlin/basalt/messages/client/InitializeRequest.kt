07f1672ea72c23ed63ab0980c470299e51f9883a
Sam <ophisbonklol@gmail.com>
2018-08-10 01:12:34 +0100

    rename key

--- src/main/kotlin/basalt/messages/client/InitializeRequest.kt
+++ src/main/kotlin/basalt/messages/client/InitializeRequest.kt
@@ -20,7 +20,7 @@ import com.jsoniter.annotation.JsonProperty
 
 @Suppress("UNUSED")
 class InitializeRequest @JsonCreator constructor(@JsonProperty("op", required = true, nullable = false) val op: String,
-                                                 @JsonProperty("guild_id", required = true, nullable = false) val guildId: String,
+                                                 @JsonProperty("guildId", required = true, nullable = false) val guildId: String,
                                                  @JsonProperty("sessionId", required = true, nullable = false) val sessionid: String,
                                                  @JsonProperty("token", required = true, nullable = false) val token: String,
                                                  @JsonProperty("endpoint", required = true, nullable = false) val endpoint: String)
